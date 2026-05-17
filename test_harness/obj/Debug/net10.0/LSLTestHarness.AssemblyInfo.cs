e287cc37b157d0f7e3fba1ca116782f9db2ddfb3
anne-skydancer <anne.skydancer@outlook.com>
2026-05-17 10:23:36 +0100

    Update AssemblyInfo and cache files for versioning consistency

--- test_harness/obj/Debug/net10.0/LSLTestHarness.AssemblyInfo.cs
+++ test_harness/obj/Debug/net10.0/LSLTestHarness.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LSLTestHarness")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23de899aa3d8156237f055b60ade18a93a5bc8f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9d3388062e6efaa4fbd55fee9bb1d329a14ccea")]
 [assembly: System.Reflection.AssemblyProductAttribute("LSLTestHarness")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LSLTestHarness")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
